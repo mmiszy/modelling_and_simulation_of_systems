504246ddea999354f21b4d0677726bf48ccc1004
Michal Miszczyszyn <michal@mmiszy.pl>
2015-10-08 15:16:47 +0200

    Add rounding

--- main.cpp
+++ main.cpp
@@ -1,23 +1,64 @@
 #include <gmpxx.h>
 #include <iostream>
 #include <stdexcept>
+#include <cstdlib>
 
-std::string getFormattedFloat(const mpf_class &value)
+using precision = int;
+
+std::string round(const std::string &str, const precision &decimals)
+{
+    if (str.length() <= decimals) {
+        return str;
+    }
+    std::string ret(str);
+    int next_decimal = str[decimals] - '0';
+    if (next_decimal >= 5) {
+        ret.replace((unsigned long) decimals - 1, 1, 1, (char) (str[decimals - 1] + 1));
+    }
+
+    return ret.substr(0, (unsigned long) decimals);
+}
+
+std::string getFormattedFloat(const mpf_class &value, const precision &d)
 {
     mp_exp_t exponent;
     std::string str = value.get_str(exponent);
+    bool isNegative = (str[0] == '-');
+
     if (value == 0.0) {
         return "0";
     } else if (exponent < 0) {
-        return "0." + std::string((unsigned long) -exponent, '0') + str;
+        if (d <= -exponent) {
+            return "0";
+        }
+
+        precision decimals = (precision) (d + exponent);
+
+        if (decimals < 0) {
+            decimals = 0;
+        }
+        str = str.substr((unsigned long) isNegative);
+
+        return (isNegative ? std::string("-") : std::string("")) + "0." + std::string((unsigned long) -exponent, '0') +
+               round(str, decimals);
     } else if (exponent == 0) {
-        return "0." + str;
+        str = str.substr((unsigned long) isNegative);
+        return (isNegative ? std::string("-") : std::string("")) + "0." + round(str, d);
     } else {
         long int length_diff = exponent - str.length();
         if (length_diff > 0) {
             return str + std::string((unsigned long) length_diff, '0');
         } else if (length_diff < 0) {
-            return str.insert(str.length() + length_diff, ".");
+            precision decimals = (precision) (str.length() + length_diff + d + isNegative);
+            precision decimal_point_position = (precision) (str.length() + length_diff + isNegative);
+
+            str = round(str, decimals);
+
+            if (decimal_point_position == str.length()) {
+                return str;
+            }
+
+            return str.insert((unsigned long) decimal_point_position, ".");
         } else {
             return str;
         }
@@ -26,14 +67,13 @@ std::string getFormattedFloat(const mpf_class &value)
 
 int main(int argc, char *argv[])
 {
-    int d;
+    precision d;
 
     if (argc < 2) {
-        d = 9999;
+        d = 7;
     } else {
         d = std::stoi(argv[1]);
     }
-
 
     int n = 0;
     mpf_set_default_prec(640);
@@ -53,7 +93,7 @@ int main(int argc, char *argv[])
     mpf_class mean_value;
     mean_value = mpf_class(accumulator) / n;
 
-    std::cout << getFormattedFloat(mean_value) << std::endl;
+    std::cout << getFormattedFloat(mean_value, d) << std::endl;
 
     return 0;
 }
